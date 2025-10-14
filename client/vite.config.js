a3d35725c0b6801c41ce25e2a1c34ae57af3f3f5
saiesha-m <saiesha21052@gmail.com>
2025-10-14 10:41:07 +0530

    feat: added base for proper routing

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,9 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ensures paths resolve correctly on deployment
   optimizeDeps: {
     include: ['@apollo/client', 'graphql'],
   },
