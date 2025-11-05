6dcd70a3ad1db65b808726a7e49f5e07d8aedbd9
Saiesha <saiesha21052@gmail.com>
2025-11-05 13:02:54 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import client from "./apolloClient";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import * as Sentry from "@sentry/react";
 
-// Initialize Sentry
+
 Sentry.init({
   dsn: "https://319261d8c3358d1cd9d9a6d24da78fff@o4510243783049216.ingest.us.sentry.io/4510243802710016",
   tracesSampleRate: 1.0,
